8d3ea01bb5ad9fca67b094722688e56ec9678144
Gia Huy <nguyengiahuy16@gmail.com>
2020-10-20 20:40:59 +0700

    Test use google drive

--- src/index.ts
+++ src/index.ts
@@ -107,23 +107,68 @@ const init = async () => {
 
 import Request from 'request';
 import { Stream } from 'stream';
+import Fs from 'fs';
 
-const t =
-  'https://drive.google.com/uc?export=download&id=1UJcmTfdIHH4vRPeICsdqeAUApnkaFXWm';
+const folder =
+  'https://drive.google.com/drive/u/2/mobile/folders/1ivE5BglKZ4XN81ydclY8PRu6WxJmspQE';
+
+const getLinkDownload = (id: string) => {
+  return `https://drive.google.com/uc?export=download&id=${id}`;
+};
+
+const hexToAscii = (str: string): string => {
+  return str
+    .replace(/\\x.{1,2}/g, (v) => {
+      const hex = v.replace('\\x', '');
+      return String.fromCharCode(parseInt(hex, 16));
+    })
+    .replace(/\\n/g, '');
+};
+
+const getAllFolder = async (link: string) => {
+  return new Promise((resolve, reject) => {
+    Request(link, (err, content) => {
+      if (err) {
+        reject(err);
+      }
+
+      const str = content.body
+        .split(`<script>window['_DRIVE_ivd'] = '`)[1]
+        .split(`';if`)[0];
+
+      Fs.writeFileSync(Path.join(__dirname, 'a.json'), hexToAscii(str));
+
+      const arr = JSON.parse(hexToAscii(str));
+      const files = arr[0].map((file: unknown[]) => {
+        const id = file[0];
+        const filename = file[2];
+        return { id, filename };
+      });
+
+      resolve(files);
+    });
+  });
+};
 
 const test = async (server: Hapi.Server) => {
-  server.route({
-    method: 'GET',
-    path: '/public/decode/test/playlist.m3u8',
-    options: {
-      auth: false
-    },
-    handler: function (request, h) {
-      const channel = new Stream.PassThrough();
-      Request(t).pipe(channel);
-      return h.response(channel);
-    }
+  const files = (await getAllFolder(folder)) as [];
+
+  files.map((o: { id: string; filename: string }) => {
+    server.route({
+      method: 'GET',
+      path: `/public/decode2/test/${o.filename}`,
+      options: {
+        auth: false
+      },
+      handler: function (request, h) {
+        const channel = new Stream.PassThrough();
+        Request(getLinkDownload(o.id)).pipe(channel);
+        return h.response(channel);
+      }
+    });
   });
+
+
 };
 
 process.on('unhandledRejection', (err) => {

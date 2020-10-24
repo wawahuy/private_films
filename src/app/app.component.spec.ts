0299a9f372cae013ee36e4eb5f4528cec31b1ea7
Gia Huy <nguyengiahuy16@gmail.com>
2020-10-25 03:46:23 +0700

    fe to csr

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'feuser'`, () => {
+  it(`should have as title 'cc'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('feuser');
+    expect(app.title).toEqual('cc');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('feuser app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('cc app is running!');
   });
 });

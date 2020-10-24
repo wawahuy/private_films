a827192704b7d2b39e33b28433859e91b6a773ad
Gia Huy <nguyengiahuy16@gmail.com>
2020-10-25 01:43:39 +0700

    fix

--- push-git.bat
+++ push-git.bat
@@ -4,5 +4,5 @@ git add *
 git commit -m "%id%"
 git push origin main
 git subtree push --prefix server_proxy origin server_proxy
-git subtree push --prefix feuser origin feuser
+git subtree push --prefix fe_user origin fe_user
 set /p e="[enter] exit"

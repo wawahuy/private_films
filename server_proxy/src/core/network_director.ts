c17f70a28997a943516b0cf206f229c360e8e9fb
Gia Huy <nguyengiahuy16@gmail.com>
2020-10-24 22:36:03 +0700

    complete network service priority

--- server_proxy/src/core/network_director.ts
+++ server_proxy/src/core/network_director.ts
@@ -1,35 +1,138 @@
+import requestPromise from 'request-promise';
 import { IKeyPair } from '../interface/IKeyPair';
-import DupAVL from './avl_dup';
+import { AvlDataFind, Data, DataResult } from './avl_data_find';
 import { IRequest, RequestCached } from './network';
 
-export type UserDataPriority = IKeyPair<number>;
-
-export interface Node<T extends IRequest> {
-  request: T;
-  countRequest: number;
-  userData: UserDataPriority;
+export enum CompairPriority {
+  AMOUNT_MAX,
+  AMOUNT_MIN,
+  VALUE_MAX,
+  VALUE_MIN
 }
 
-export interface UserDataPriorityAVL {
-  [field: string]: DupAVL<string, number>;
+export interface NetworkCachedUserData extends Data {
+  countRequest?: number;
+  uriID?: string;
 }
+
+export interface NetworkCachedNode extends NetworkCachedUserData {
+  request: RequestCached;
+}
+
+export const DEFAULT_MINIMUM_PRIORITY = {
+  countRequest: CompairPriority.VALUE_MIN
+};
 
 export class NetworkCachedDirector {
-  static readonly USER_DATA_COUNT = '__countRequest__';
-  private cacheds: Node<RequestCached>[];
-  private sizeCachedMax: number;
-  private sizeCachedCurrent: number;
-  private orderOfPriority: string[];
-  private avlPriority: UserDataPriorityAVL;
+  private _avlRequestCached: AvlDataFind<NetworkCachedNode>;
+  private _priorityMinimumRequestFields: IKeyPair<CompairPriority>;
 
   constructor() {
-    this.cacheds = [];
-    this.sizeCachedMax = 300 * 1024 * 1024;
-    this.sizeCachedCurrent = 0;
-    this.orderOfPriority = [];
-    this.avlPriority = {};
+    this._avlRequestCached = new AvlDataFind();
+    this._avlRequestCached.exceptFields(['request']);
+    this._priorityMinimumRequestFields = DEFAULT_MINIMUM_PRIORITY;
   }
 
-  push(request: RequestCached, userDataPriority?: UserDataPriority) {}
-  setOrderOfPriority(fields: string[]) {}
+  get size() {
+    return 0;
+  }
+
+  get datas() {
+    return this._avlRequestCached.datas;
+  }
+
+  find(userDataField: NetworkCachedUserData) {
+    const fields = Object.keys(userDataField);
+    let avlCurrent: AvlDataFind<NetworkCachedNode> | undefined = this
+      ._avlRequestCached;
+    let avlResult: DataResult<NetworkCachedNode> | null | undefined;
+    let field = fields.shift();
+    while (field) {
+      avlResult = avlCurrent?.find(field, userDataField[field]);
+      field = fields.shift();
+      if (field) {
+        avlCurrent = avlResult?.nextAvl;
+      }
+    }
+    return avlResult?.data;
+  }
+
+  insert(
+    request: RequestCached,
+    userDataFields?: NetworkCachedUserData
+  ): NetworkCachedNode {
+    const node: NetworkCachedNode = {
+      uriID: this.getUriID(request),
+      request,
+      countRequest: 1,
+      ...(userDataFields || {})
+    };
+    this._avlRequestCached.insert(node);
+    return node;
+  }
+
+  update(uriID: string, userDataFields?: NetworkCachedUserData) {
+    const nodes = this.find({ uriID });
+    if (nodes && nodes.length) {
+      this._avlRequestCached.remove(nodes[0]);
+      this._avlRequestCached.insert({ ...nodes[0], ...(userDataFields || {}) });
+    }
+  }
+
+  remove(uriID: string) {
+    const nodes = this.find({ uriID });
+    if (nodes && nodes.length) {
+      this._avlRequestCached.remove(nodes[0]);
+    }
+  }
+
+  setMinimumPriority(pr: IKeyPair<CompairPriority>) {
+    this._priorityMinimumRequestFields = pr;
+  }
+
+  minimumRequsetPriority() {
+    const fields = Object.keys(this._priorityMinimumRequestFields);
+    let avlCurrent: AvlDataFind<NetworkCachedNode> | undefined = this
+      ._avlRequestCached;
+    let avlResult: DataResult<NetworkCachedNode> | null | undefined;
+    let field = fields.shift();
+
+    while (field) {
+      const type = this._priorityMinimumRequestFields[field];
+      switch (type) {
+        case CompairPriority.AMOUNT_MAX:
+          avlResult = avlCurrent?.maxOfNumberValue(field);
+          break;
+        case CompairPriority.AMOUNT_MIN:
+          avlResult = avlCurrent?.minOfNumberValue(field);
+          break;
+        case CompairPriority.VALUE_MAX:
+          avlResult = avlCurrent?.max(field);
+          break;
+        case CompairPriority.VALUE_MIN:
+          avlResult = avlCurrent?.min(field);
+          break;
+      }
+
+      field = fields.shift();
+      if (field) {
+        avlCurrent = avlResult?.nextAvl;
+      }
+    }
+
+    if (avlResult?.data.length) {
+      return avlResult.data[0];
+    }
+    return null;
+  }
+
+  getUriID(request: RequestCached) {
+    const rq = request.requestNative;
+    const key =
+      (rq?.uri.href || '') +
+      (rq?.method || '') +
+      JSON.stringify(rq?.body || {}) +
+      JSON.stringify(rq?.form || {});
+    return key;
+  }
 }
--- server_proxy/src/services/network_service.ts
+++ server_proxy/src/services/network_service.ts
@@ -1,17 +1,21 @@
 import { EventEmitter } from 'events';
 import LibRequest, { Headers } from 'request';
 import RequestPromise from 'request-promise';
-import AVL from 'avl';
 import { IRequest, Request, RequestCached } from '../core/network';
+import {
+  CompairPriority,
+  DEFAULT_MINIMUM_PRIORITY,
+  NetworkCachedDirector,
+  NetworkCachedUserData
+} from '../core/network_director';
 import { IKeyPair } from '../interface/IKeyPair';
 
-export type NetworkCachedPriority = IKeyPair<string | number>;
 export type NetworkCallback = LibRequest.RequestCallback;
 export type NetworkResponse = LibRequest.Response;
 export type NetworkOptions = {
   callback?: NetworkCallback;
   skipCached?: boolean;
-  dataComputePriority?: NetworkCachedPriority;
+  userData?: NetworkCachedUserData;
 } & RequestPromise.RequestPromiseOptions;
 
 declare interface NetworkService {
@@ -23,12 +27,12 @@ class NetworkService extends EventEmitter {
   readonly HEADERS: Headers = {
     'User-Agent': `Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Mobile Safari/537.36`
   };
-  readonly MAX_REQUEST_SAVED = 200 * 1024 * 1024;
-  readonly NAME_AVL_NUM_PRIORITY = '___priority';
 
-  private cacheds: IKeyPair<IRequest> = {};
+  readonly MAX_REQUEST_SAVED = 300 * 1024 * 1024;
+  readonly MAX_TIME_PRIORITY = 2 * 60 * 60 * 1000;
+
   private sizeByte;
-  private cachedsV2: IRequest[];
+  private networkCachedDirector: NetworkCachedDirector;
   private static _instance: NetworkService;
 
   static get instance() {
@@ -41,15 +45,26 @@ class NetworkService extends EventEmitter {
   constructor() {
     super();
     this.sizeByte = 0;
-    this.cachedsV2 = [];
+    this.networkCachedDirector = new NetworkCachedDirector();
+    this.networkCachedDirector.setMinimumPriority({
+      time: CompairPriority.VALUE_MIN,
+      filmID: CompairPriority.AMOUNT_MIN,
+      ...DEFAULT_MINIMUM_PRIORITY
+    });
   }
 
   public get size() {
     return this.sizeByte;
   }
 
-  public get ids() {
-    return Object.keys(this.cacheds);
+  public get log() {
+    return this.networkCachedDirector.datas.map((data) => {
+      if (data) {
+        const { request, ...ret } = data;
+        return ret;
+      }
+      return null;
+    });
   }
 
   public get(uri: string, options?: NetworkOptions) {
@@ -58,13 +73,18 @@ class NetworkService extends EventEmitter {
     }
     const skipCached = options.skipCached || false;
     const callback = options.callback;
+    const userData = { time: this.timePriority(), ...(options.userData || {}) };
     const requestOptions = this.getRequestOptions(options);
     const key = this.getID(uri, 'GET', requestOptions);
     if (!skipCached) {
-      const r = this.cacheds[key];
-      if (r) {
-        this.emit('old_request', r);
-        return r;
+      const r = this.networkCachedDirector.find({ uriID: key });
+      if (r && r.length) {
+        this.networkCachedDirector.update(key, {
+          time: this.timePriority(),
+          countRequest: (r[0]?.countRequest || 1) + 1
+        });
+        this.emit('old_request', r[0].request);
+        return r[0].request;
       }
     }
 
@@ -78,7 +98,7 @@ class NetworkService extends EventEmitter {
     const requestClazz = skipCached ? Request : RequestCached;
     const requestCached = new requestClazz(request);
     if (!skipCached) {
-      this.saveCache(key, requestCached);
+      this.saveCache(key, requestCached, userData || {});
     }
     this.emit('new_request', requestCached);
     return requestCached;
@@ -90,13 +110,18 @@ class NetworkService extends EventEmitter {
     }
     const skipCached = options.skipCached || false;
     const callback = options.callback;
+    const userData = { time: this.timePriority(), ...(options.userData || {}) };
     const requestOptions = this.getRequestOptions(options);
     const key = this.getID(uri, 'POST', requestOptions);
     if (!skipCached) {
-      const r = this.cacheds[key];
-      if (r) {
-        this.emit('old_request', r);
-        return r;
+      const r = this.networkCachedDirector.find({ uriID: key });
+      if (r && r.length) {
+        this.networkCachedDirector.update(key, {
+          time: this.timePriority(),
+          countRequest: (r[0]?.countRequest || 1) + 1
+        });
+        this.emit('old_request', r[0].request);
+        return r[0].request;
       }
     }
 
@@ -110,42 +135,37 @@ class NetworkService extends EventEmitter {
     const requestClazz = skipCached ? Request : RequestCached;
     const requestCached = new requestClazz(request);
     if (!skipCached) {
-      this.saveCache(key, requestCached);
+      this.saveCache(key, requestCached, userData || {});
     }
     this.emit('new_request', requestCached);
     return requestCached;
   }
 
-  private saveCache(key: string, requestCached: RequestCached) {
+  private saveCache(
+    key: string,
+    requestCached: RequestCached,
+    userData: NetworkCachedUserData
+  ) {
     this.popIfFull();
-    this.cacheds[key] = requestCached;
+    const uData: NetworkCachedUserData = {
+      ...userData,
+      uriID: key
+    };
+    this.networkCachedDirector.insert(requestCached, uData);
     requestCached.once('end', () => {
       this.sizeByte += requestCached.getByteLength();
     });
-    requestCached.once('abort', () => this.remove(key));
-    requestCached.once('error', () => this.remove(key));
-  }
-
-  private remove(id: string) {
-    if (this.cacheds[id]) {
-      this.sizeByte -= this.cacheds[id].getByteLength();
-    }
-    delete this.cacheds[id];
-  }
-
-  private pop() {
-    const key = this.ids.shift() || '';
-    if (key) {
-      const request = this.cacheds[key];
-      this.remove(key);
-      return request;
-    }
-    return null;
+    requestCached.once('abort', () => this.networkCachedDirector.remove(key));
+    requestCached.once('error', () => this.networkCachedDirector.remove(key));
   }
 
   private popIfFull() {
-    if (this.size > this.MAX_REQUEST_SAVED) {
-      return this.pop();
+    if (this.sizeByte > this.MAX_REQUEST_SAVED) {
+      const badReq = this.networkCachedDirector.minimumRequsetPriority();
+      if (badReq?.uriID) {
+        this.sizeByte -= badReq.request.getByteLength();
+        this.networkCachedDirector.remove(badReq.uriID);
+      }
     }
     return null;
   }
@@ -156,6 +176,10 @@ class NetworkService extends EventEmitter {
     options?: RequestPromise.RequestPromiseOptions
   ) {
     return uri + JSON.stringify(options || {}) + method;
+  }
+
+  private timePriority() {
+    return new Date().getTime() + this.MAX_TIME_PRIORITY;
   }
 
   private getRequestOptions(

399c94b79fc481becc56f9b3ba3a9e7c429cacd4
Gia Huy <nguyengiahuy16@gmail.com>
2020-10-28 03:47:09 +0700

    fix

--- server_manager/bash-deploy.sh
+++ server_manager/bash-deploy.sh
@@ -1,6 +1,10 @@
 docker-compose stop
 docker-compose rm -f
 
+# fix
+mkdir -p .docker/data/mongodb
+sudo chmod -R go+w .docker/data/mongodb
+
 docker rmi private_films/server_manager:lasted || true
 docker tag private_films/server_manager:build private_films/server_manager:lasted
 

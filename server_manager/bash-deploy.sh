1e434debe71f65c732d6e59cd4c1833ddd4d41cc
Gia Huy <nguyengiahuy16@gmail.com>
2020-10-28 03:19:01 +0700

    fix

--- server_manager/bash-deploy.sh
+++ server_manager/bash-deploy.sh
@@ -1,7 +1,3 @@
-DIR="~/sever_manager"
-cd "$DIR"
-chmod u+x ./bash-deploy.sh && ./bash-deploy.sh
-
 docker-compose stop
 docker-compose rm -f
 docker-compose up -d
--- server_manager/bash-pre-deploy.sh
+++
@@ -1,5 +0,0 @@
-DIR="~/sever_manager"
-mkdir -p "$DIR"
-cd "$DIR"
-rm ./docker-compose.yml
-rm ./bash-deploy.sh

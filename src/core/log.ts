c571e75776e60a00bed63cfed8fc264aec68f695
Gia Huy <nguyengiahuy16@gmail.com>
2020-10-29 01:00:55 +0700

    setup services manager

--- src/core/log.ts
+++ src/core/log.ts
@@ -3,3 +3,7 @@ export async function logTimer(name: string, func: () => void) {
   await func();
   console.log(name, new Date().getTime() - t);
 }
+
+export function log(...args: unknown[]) {
+  console.log(...args);
+}
---
+++ src/services/mongo_service.ts
@@ -0,0 +1,53 @@
+import Mongoose, { Schema } from 'mongoose';
+import { log } from '../core/log';
+
+class MongoService {
+  private static _instance: MongoService;
+
+  static get instance() {
+    if (!MongoService._instance) {
+      MongoService._instance = new MongoService();
+    }
+    return MongoService._instance;
+  }
+
+  private _clients: Mongoose.Mongoose | undefined;
+
+  constructor() {}
+
+  private get clients() {
+    return this._clients;
+  }
+
+  private onConnect() {
+    log('[Mongo]', process.env.MONGO_URI);
+    log('[Mongo] connection!');
+  }
+
+  private onError(err: Mongoose.Error) {
+    log('[Mongo] error!');
+    log(err);
+  }
+
+  public async establish() {
+    this._clients = await Mongoose.connect(
+      process.env.MONGO_URI as string,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+      },
+      (err) => {
+        if (err) {
+          this.onError(err);
+        } else {
+          this.onConnect();
+        }
+      }
+    );
+    Mongoose.Promise = global.Promise;
+  }
+}
+
+export default MongoService;

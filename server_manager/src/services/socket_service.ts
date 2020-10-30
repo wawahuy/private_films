3b59b518c17cce6a729e0070574d1a6a29233da4
Gia Huy <nguyengiahuy16@gmail.com>
2020-10-30 11:38:40 +0700

    test

--- server_manager/src/services/socket_service.ts
+++ server_manager/src/services/socket_service.ts
@@ -4,10 +4,19 @@ import { Socket } from 'net';
 import Ws from 'ws';
 import Url from 'url';
 import { log } from '../core/log';
+import { IKeyPair } from '../interface/IKeyPair';
+
+export type SocketSendOptions = {
+  mask?: boolean;
+  binary?: boolean;
+  compress?: boolean;
+  fin?: boolean;
+};
 
 class SocketService {
   private static _instance: SocketService;
   private wss!: Ws.Server;
+  readonly path = '/ws/manager';
 
   static get instance() {
     if (!SocketService._instance) {
@@ -16,15 +25,20 @@ class SocketService {
     return SocketService._instance;
   }
 
+  get server() {
+    return this.wss;
+  }
+
   async establish(server: Hapi.Server) {
     this.wss = new Ws.Server({ noServer: true });
     this.wss.on('connection', this.onConnection.bind(this));
     server.listener.on('upgrade', this.onUpgrade.bind(this));
+    log('[Socket]', `Socket listening at ${this.path}`);
   }
 
   private onUpgrade(request: IncomingMessage, socket: Socket, head: Buffer) {
     const pathname = Url.parse(request.url || '').pathname;
-    if (pathname === '/ws/manager') {
+    if (pathname === this.path) {
       this.wss.handleUpgrade(request, socket, head, (ws) => {
         this.wss.emit('connection', ws, request);
       });
@@ -34,7 +48,27 @@ class SocketService {
   }
 
   private onConnection(socket: Ws, request: IncomingMessage) {
-    log('new connect!');
+    socket.on('message', this.onMessage.bind(this, socket));
+    socket.on('close', this.onClose.bind(this, socket));
+    socket.on('error', this.onError.bind(this, socket));
+  }
+
+  private onMessage(socket: Ws, data: Ws.Data) {
+    this.broadcast(data);
+  }
+
+  private onClose(socket: Ws, code: number, reason: string) {}
+
+  private onError(socket: Ws, err: Error) {}
+
+  broadcast(
+    data: Ws.Data,
+    options?: SocketSendOptions,
+    cb?: (err?: Error) => void
+  ) {
+    this.wss.clients.forEach((socket: Ws) => {
+      socket.send(data, options || {}, cb);
+    });
   }
 }
 
